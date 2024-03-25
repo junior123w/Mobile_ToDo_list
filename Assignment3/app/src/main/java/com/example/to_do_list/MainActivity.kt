7bad3d2557709fa25a58d77b13a40e0685e755ce
junior123w <113151392+junior123w@users.noreply.github.com>
2024-03-24 22:52:04 -0400

    Assignment3 completed

--- Assignment3/app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ Assignment3/app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -4,14 +4,31 @@ package com.example.to_do_list
 * Student id : 200522618
 * File name: Main activity
 * Date:24-03-2024
-*  Description : This is a task tracker app that will keep of task that you can create edit adn delete
-* Version : V 0.3*/
+*  Description : This is a task tracker app that will keep of task that you can create edit and delete
+* Version : V 1.0*/
+
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.to_do_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var context :Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        context = this // Initializing context with the current activity's context
+
+        //added  intent for FAB so that it can access the other xml file
+        binding.addTaskFAB.setOnClickListener{
+            val intent = Intent(context, TodoDetailsActivity::class.java)
+            context.startActivity(intent)
+        }
     }
+
 }
---
+++ Assignment3/app/src/main/java/com/example/to_do_list/TodoDetailsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.to_do_list
+/*
+* Name: Naimil Shah
+* Student id : 200522618
+* File name: ToDoDetailsActivity
+* Date:24-03-2024
+*  Description : This is a task tracker app that will keep of task that you can create edit and delete
+* Version : V 1.0*/
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.to_do_list.databinding.ActivityTodoDetailsBinding
+//created a mock file so that I can see the other xml file
+class TodoDetailsActivity:AppCompatActivity() {
+    private lateinit var binding: ActivityTodoDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTodoDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Closing TodoDetailsActivity when "Cancel" button is clicked
+        binding.cancelButton.setOnClickListener { finish() }
+    }
+}
