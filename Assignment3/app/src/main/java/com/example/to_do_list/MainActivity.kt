952979526b2604d3b4d05939d88efedd355c3895
junior123w <113151392+junior123w@users.noreply.github.com>
2024-04-04 19:47:12 -0400

    updated UI

--- Assignment3/app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ Assignment3/app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -7,28 +7,23 @@ package com.example.to_do_list
 *  Description : This is a task tracker app that will keep of task that you can create edit and delete
 * Version : V 1.0*/
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.content.Intent
 import com.example.to_do_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var context :Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        context = this // Initializing context with the current activity's context
-
-        //added  intent for FAB so that it can access the other xml file
+        // Use 'this' directly to start the new activity
         binding.addTaskFAB.setOnClickListener{
-            val intent = Intent(context, TodoDetailsActivity::class.java)
-            context.startActivity(intent)
+            val intent = Intent(this, TodoDetailsActivity::class.java)
+            startActivity(intent)
         }
     }
-
 }
