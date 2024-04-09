76a6194ee04b875784471aa2c64c3e5bf559897f
junior123w <113151392+junior123w@users.noreply.github.com>
2024-04-08 23:24:40 -0400

    final commit

--- Assignment4/app/src/main/java/com/example/to_do_list2/MainActivity.kt
+++ Assignment4/app/src/main/java/com/example/to_do_list2/MainActivity.kt
@@ -4,7 +4,9 @@ package com.example.to_do_list2
 * Student id : 200522618
 * File name: Main activity
 * Date:08-04-2024
-*  Description : This is a task tracker app that will keep of task that you can create
+*  Description : This is a task tracker app that will keep of task that you can create was
+* not able to get the details only that name and due date can be seen and task can be strikethroughed
+* if completed can be done
 * Version : V 2.0*/
 
 import android.content.Context
